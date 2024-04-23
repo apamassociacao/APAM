7c21cfc61494194aa8e7e15262f6a4208b078ab9
Jan Lucas <janlucas13@gmail.com>
2024-04-23 20:41:21 -0300

    criando componente footer

--- src/Components/BaseLayout/BaseLayout.tsx
+++ src/Components/BaseLayout/BaseLayout.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 
 import Header from '../Header/Header.tsx';
 import { Outlet } from "react-router-dom";
+import Footer from '../Footer/Footer.tsx';
 
 const BaseLayout: FC = () => (
     <>
         <Header />
         <Outlet />
+        <Footer />
     </>
 );
 
