71e04282339cec629ecdb46c98921597a355b2a1
Jan Lucas <janlucas13@gmail.com>
2024-06-01 19:42:13 -0300

    feat: put dog and cat img

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -3,12 +3,17 @@ import './_aboutUs.scss';
 const AboutUs: React.FC = () => (
   <section className="about-us">
     <div className="about-us__container">
+      <div className="about-us__container__imgs">
+        <img src="/images/dog1.png" alt="Cachorro com lingua pra fora" />
+        <img src="/images/cat1.png" alt="Gato dourado deitado" />
+      </div>
       <div className="about-us__container__body">
         <div className="about-us__container__body__left">
           <h2>Quem somos</h2>
           <p>
-            Somos a Associação Protetora dos Animais de Mato Grosso (APAM), uma
-            organização dedicada ao resgate, cuidado e adoção de animais em
+            Somos a{' '}
+            <span>Associação Protetora dos Animais de Mato Grosso (APAM)</span>,
+            uma organização dedicada ao resgate, cuidado e adoção de animais em
             situações vulneráveis. Nossa missão é proporcionar um lar seguro e
             amoroso para cada ser que cruzar nosso caminho.
           </p>
