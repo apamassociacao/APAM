7eb0a1c8e11545fdb16120f0f3523947927d44ff
Jan Lucas <janlucas13@gmail.com>
2024-06-02 14:14:28 -0300

    feat: add button cards and styles

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -1,4 +1,5 @@
 import './_aboutUs.scss';
+import Button from '@/components/Button/Button.tsx';
 
 const AboutUs: React.FC = () => (
   <section className="about-us">
@@ -17,10 +18,40 @@ const AboutUs: React.FC = () => (
             situações vulneráveis. Nossa missão é proporcionar um lar seguro e
             amoroso para cada ser que cruzar nosso caminho.
           </p>
+          <div className="about-us__container__body__button">
+            <Button className="about-us__container__body__button__about">
+              <a href="#" target="_blank">
+                Conheça nosso instagram
+              </a>
+            </Button>
+            <Button className="about-us__container__body__button__contact">
+              <a href="#" target="_blank">
+                Contate-nos no email
+              </a>
+            </Button>
+          </div>
         </div>
-        <div className="about-us__container__body__rigth">
+        <div className="about-us__container__body__right">
           <h2>Nossa missão</h2>
           <p>Além do resgate e do incentivo a adoção, nós também:</p>
+          <div className="about-us__container__body__button">
+            <Button className="about-us__container__body__button-card">
+              <img src="/images/Pata.svg" alt="Ícone de patinha" />
+              Defendemos os animais.
+            </Button>
+            <Button className="about-us__container__body__button-card">
+              <img src="/images/Pata.svg" alt="Ícone de patinha" />
+              Lutamos por políticas públicas em prol dos animais.
+            </Button>
+            <Button className="about-us__container__body__button-card">
+              <img src="/images/Pata.svg" alt="Ícone de patinha" />
+              Zelamos pelo meio-ambiente.
+            </Button>
+            <Button className="about-us__container__body__button-card">
+              <img src="/images/Pata.svg" alt="Ícone de patinha" />
+              Incentivamos a sociedade na proteção à flora e a fauna.
+            </Button>
+          </div>
         </div>
       </div>
     </div>
