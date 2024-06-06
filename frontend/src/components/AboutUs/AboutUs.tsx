c5d1da542bac78528a8254dda7a5328579d81b5c
Jan Lucas <janlucas13@gmail.com>
2024-06-05 23:11:12 -0300

    refactor:  adjust component Button for Card

--- frontend/src/components/AboutUs/AboutUs.tsx
+++ frontend/src/components/AboutUs/AboutUs.tsx
@@ -1,7 +1,7 @@
 import './_aboutUs.scss';
-import Button from '@/components/Button/Button.tsx';
 import IconInsta from '../../../public/images/bi_instagram.svg';
 import IconEmail from '../../../public/images/uil_copy.svg';
+import Card from '../Card/Card';
 
 const AboutUs: React.FC = () => (
   <section className="about-us">
@@ -23,7 +23,7 @@ const AboutUs: React.FC = () => (
             amoroso para cada ser que cruzar nosso caminho.
           </p>
           <div className="about-us__container__body__button">
-            <Button className="about-us__container__body__button__about">
+            <Card className="about-us__container__body__button__about">
               <a
                 href="https://www.instagram.com/apammt?igsh=a2lyNjRwdXNkOHg3"
                 target="_blank"
@@ -32,8 +32,8 @@ const AboutUs: React.FC = () => (
                 <img src={IconInsta} alt="Ícone instagram" />
                 Conheça nosso instagram
               </a>
-            </Button>
-            <Button className="about-us__container__body__button__contact">
+            </Card>
+            <Card className="about-us__container__body__button__contact">
               <a
                 href="mailto:apammt@gmail.com"
                 target="_blank"
@@ -42,29 +42,29 @@ const AboutUs: React.FC = () => (
                 <img src={IconEmail} alt="Ícone Email" />
                 Contate-nos no email
               </a>
-            </Button>
+            </Card>
           </div>
         </div>
         <div className="about-us__container__body__right">
           <h2>Nossa missão</h2>
           <p>Além do resgate e do incentivo a adoção, nós também:</p>
           <div className="about-us__container__body__button">
-            <Button className="about-us__container__body__button-card">
+            <Card className="about-us__container__body__button-card">
               <img src="/images/Pata.svg" alt="Ícone de patinha" />
               Defendemos os animais.
-            </Button>
-            <Button className="about-us__container__body__button-card">
+            </Card>
+            <Card className="about-us__container__body__button-card">
               <img src="/images/Pata.svg" alt="Ícone de patinha" />
               Lutamos por políticas públicas em prol dos animais.
-            </Button>
-            <Button className="about-us__container__body__button-card">
+            </Card>
+            <Card className="about-us__container__body__button-card">
               <img src="/images/Pata.svg" alt="Ícone de patinha" />
               Zelamos pelo meio-ambiente.
-            </Button>
-            <Button className="about-us__container__body__button-card">
+            </Card>
+            <Card className="about-us__container__body__button-card">
               <img src="/images/Pata.svg" alt="Ícone de patinha" />
               Incentivamos a sociedade na proteção à flora e a fauna.
-            </Button>
+            </Card>
           </div>
         </div>
       </div>
