4ae2fadeb7e6dbccb8af069f32ce449a0ce22127
Marcos Bião <dev.marcosdly@gmail.com>
2024-07-17 02:46:05 -0300

    fix(AdocaoCard): incorrect typing, generic prop isn't generic

--- frontend/src/components/AdocaoCard/AdocaoCard.tsx
+++ frontend/src/components/AdocaoCard/AdocaoCard.tsx
@@ -3,7 +3,7 @@ import './_adocaoCard.scss';
 
 interface AdocaoCardProps {
   titulo: string;
-  idade: string;
+  idade: number;
   imageUrl?: string;
 }
 
@@ -14,7 +14,9 @@ const AdocaoCard: FC<AdocaoCardProps> = ({ titulo, idade, imageUrl }) => {
       <div className="adocaocard__bottom-container">
         <div className="adocaocard__info-container">
           <p className="adocaocard__animal-name">{titulo}</p>
-          <p className="adocaocard__animal-age">{idade}</p>
+          <p className="adocaocard__animal-age">
+            {idade <= 1 ? '1 Ano' : `${idade} Anos`}
+          </p>
         </div>
         <img className="adocaocard__paw-icon" src="/images/fa-paw.png" />
       </div>
