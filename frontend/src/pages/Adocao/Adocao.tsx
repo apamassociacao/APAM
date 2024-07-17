cf81f30be224eb3540a1c3d535aba646e559387d
Marcos Bião <dev.marcosdly@gmail.com>
2024-07-17 01:40:33 -0300

    refactor(Adocao): using filter state provider

--- frontend/src/pages/Adocao/Adocao.tsx
+++ frontend/src/pages/Adocao/Adocao.tsx
@@ -5,68 +5,88 @@ import cachorro from '../../../public/images/cachorro.png';
 import Header from '@/components/Header/Header';
 import Paginacao from '@/components/Paginacao/Paginacao';
 import FilterButton from '@/components/FilterButton/FilterButton';
-import { FC, useState } from 'react';
-
+import { FC } from 'react';
+import {
+  AdocaoFilterProvider,
+  FilterOption,
+} from '@/hooks/AdocaoFilterProvider';
 import './_adocao.scss';
 
 const cardsData = [
-  { id: 1, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
+  {
+    id: 1,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
   {
     id: 2,
     titulo: 'Cachorrin',
     idade: '2 anos',
     imageUrl: cachorro,
-    tipo: 'cachorro',
+    tipo: FilterOption.dog,
   },
-  { id: 3, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
-  { id: 4, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
-  { id: 5, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
-  { id: 6, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
-  { id: 7, titulo: 'Gatin', idade: '1 ano', imageUrl: gato, tipo: 'gato' },
+  {
+    id: 3,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
+  {
+    id: 4,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
+  {
+    id: 5,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
+  {
+    id: 6,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
+  {
+    id: 8,
+    titulo: 'Gatin',
+    idade: '1 ano',
+    imageUrl: gato,
+    tipo: FilterOption.cat,
+  },
 ];
 
-const Adocao: FC<unknown> = () => {
-  const [filter, setFilter] = useState('all');
-
-  const handleFilterChange = (newFilter: string) => {
-    setFilter(newFilter);
-  };
-
-  const filteredItems =
-    filter === 'all'
-      ? cardsData
-      : cardsData.filter((item) => item.tipo === filter);
-
-  return (
-    <main className="adocao">
-      <SidebarProvider>
-        <Sidebar />
-        <section className="adocao">
-          <div className="adocao__container">
-            <Header />
+const Adocao: FC<unknown> = () => (
+  <main className="adocao">
+    <SidebarProvider>
+      <Sidebar />
+      <section className="adocao">
+        <div className="adocao__container">
+          <Header />
+          <AdocaoFilterProvider>
             <div className="adocao__container__body">
               <h1 className="adocao__container__body__title">
                 Busque por categoria
               </h1>
               <div className="adocao__container__body__title__subtitle">
-                <FilterButton
-                  onClick={() => setFilter('gato')}
-                  label="Gatos"
-                  isActive={filter === 'gato'}
-                />
-                <FilterButton
-                  onClick={() => handleFilterChange('cachorro')}
-                  label="Cachorros"
-                  isActive={filter === 'cachorro'}
-                />
+                <FilterButton label="Gatos" filterType="cat" />
+                <FilterButton label="Cachorros" filterType="dog" />
               </div>
-              <Paginacao items={filteredItems} itemsPerPage={3} />
+              <Paginacao items={cardsData} itemsPerPage={3} />
             </div>
-          </div>
-        </section>
-      </SidebarProvider>
-    </main>
-  );
-};
+          </AdocaoFilterProvider>
+        </div>
+      </section>
+    </SidebarProvider>
+  </main>
+);
 
 export default Adocao;
